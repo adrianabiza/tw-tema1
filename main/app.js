dfd3bad02cb2392942b496eb741f395cf69db1ac
unknown <adrianabiza14@gmail.com>
2020-10-29 22:43:36 +0200

    task from todo

--- main/app.js
+++ main/app.js
@@ -1,5 +1,26 @@
 function removeOrderItem(orderInfo, position){
     
+    if(!Array.isArray(orderInfo.items)){ 
+        throw 'Items should be an array';
+    }
+
+    for(let i=0;i<orderInfo.items.lenght;i++){
+        if(orderInfo.items[i].price===undefined || orderInfo.items[i].quantity===undefined){
+            throw 'Malformed item';
+        }
+    }
+
+  
+    if(orderInfo.items[position]===undefined){
+        throw 'Invalid position';
+    }
+    else
+    {
+        orderInfo.total-=orderInfo.items[position].price*orderInfo.items[position].quantity
+        if(position >-1) {orderInfo.items.splice(position,1);}
+    }
+
+    return orderInfo;
 }
 
 const app = {
