8f537654f39fbad587573d0e5914a6d61d16db2c
unknown <adrianabiza14@gmail.com>
2020-10-30 19:49:07 +0200

    tasks

--- main/app.js
+++ main/app.js
@@ -1,15 +1,18 @@
 function removeOrderItem(orderInfo, position){
-    
+
     if(!Array.isArray(orderInfo.items)){ 
         throw 'Items should be an array';
     }
 
-    for(let i=0;i<orderInfo.items.lenght;i++){
-        if(orderInfo.items[i].price===undefined || orderInfo.items[i].quantity===undefined){
-            throw 'Malformed item';
-        }
-    }
+    // for(let i=0;i<orderInfo.items.lenght;i++){
+    //     if(orderInfo.items[i].price===undefined || orderInfo.items[i].quantity===undefined){
+    //         throw 'Malformed item';
+    //     }
+    // }
 
+    if(!orderInfo.items[1].hasOwnProperty('price') || !orderInfo.items[1].hasOwnProperty('quantity')){
+        throw 'Malformed item';
+    }
   
     if(orderInfo.items[position]===undefined){
         throw 'Invalid position';
